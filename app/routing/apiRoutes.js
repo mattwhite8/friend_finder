946a4b4d32ad3f379b84f528a34a32e914a1d7b4
Matt White <mattwhite1337@gmail.com>
2017-03-10 18:40:30 -0500

    wrote function to ensure all scores are int on server side

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,20 @@
 // Dependencies
 var friends = require('../data/friends.js');
 
+function matchScore(newFriend){
+	var bestMatch = [];
+	var bestScore = 0;
+	var friendsTruncated = friends.pop();
+
+};
+
+function stringToNum(newFriend) {
+	newFriend.scores.forEach(function(element, index, array) {
+		array[index] = parseInt(index);
+	});
+	return newFriend;
+};
+
 module.exports = function(app){
 	app.get('/api/friends', function(req, res) {
 		res.json(friends);
@@ -8,6 +22,7 @@ module.exports = function(app){
 
 	app.post('/api/friends', function(req, res) {
 		var newFriend = req.body;
+		stringToNum(newFriend);
 		friends.push(newFriend);
 		res.json(newFriend);
 	});
